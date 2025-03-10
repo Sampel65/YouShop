bd5b2bba50c1b59f5fa14c1a7be47341df5cf4fd
sampel65 <sampeldan65@gmail.com>
2025-03-10 23:15:40 +0100

    Initial Commit

---
+++ YouShop/YouShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouShopApp.swift
+//  YouShop
+//
+//  Created by Samson Oluwapelumi on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
